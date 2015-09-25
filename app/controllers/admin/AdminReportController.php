07fb277eec955bb6e63437469fc63f42e029acbe
Kennric <kennric@osuosl.org>
2015-09-25 11:07:10 -0700

    master should mirror existing production

--- app/controllers/admin/AdminReportController.php
+++ app/controllers/admin/AdminReportController.php
@@ -69,24 +69,117 @@ class AdminReportController extends AdminBaseController {
 		if (!$times || count($times) <= 0) {
 			return Redirect::to('admin/reports')->withErrors('No times found for that day');
 		}
-
+	
 		$school = str_replace(' ', '-', $school);
 
-		header('Content-Type: text/csv; charset=utf-8');
-		header('Content-Disposition: attachment; filename='.$school.'-'.$date.'.csv');
-
+	/*	echo "<pre>";		
+	//	print_r($times);
+		echo "</pre>";
+	*/
+	
+	// echo $times[0]->type;
+	
+	header('Content-Type: text/csv; charset=utf-8');
+	header('Content-Disposition: attachment; filename='.$school.'-'.$date.'.csv');
+ 
+	
+ 	$f = fopen('php://output', 'w');
+	
+	fputcsv($f, array('Minutes', 'Type', 'Id', 'Email'), ',');
+	 
+ 	foreach ($times as $time) {
+			
+			fputcsv($f, array($time->minutes , $time->type, $time->user_id, $time->user->email), ",");
+			
+		}
+	
+	fclose($f);
+	die();
+	
+	/*
+	for ($x = 0; $x <= count($times); $x++) {
+		
+		//fputcsv($out, array($row['time'] , $row['activity'] , $row['id'] ,$row['name']));
+		
+    	echo $times[$x]->minutes; echo $times[$x]->type;  echo $times[$x]->user_id; echo $times[$x]->user->email;
+		echo $x;
+		echo "<br/>";
+		
+	} 
+	
+	
+/*	foreach ($times as $time) {
+		
+	 echo $time->minutes;
+	 echo $time->activity();
+	 echo $time->user_id;
+	 echo $time->user->email;
+	
+	}
+*/	
+	/*
+//	$time->minutes; $time->activity();  $time->user_id; $time->user->email;
+	 
+			
+		//echo $times[0]->user_id;
+		
+		die();
+		
+	//	header('Content-Type: text/csv; charset=utf-8');
+	//	header('Content-Disposition: attachment; filename='.$school.'-'.$date.'.csv');
+		
 		$f = fopen('php://output', 'w');
-
-		fputcsv($f, array('Minutes', 'Type', 'Id', 'Email'), ',');
+	
+		$out = fopen('php://output', 'w');
+		
+		
 		foreach ($times as $time) {
 			$row = array();
+		
 			$row['time']     = $time->minutes;
 			$row['activity'] = $time->activity();
 			$row['id']       = $time->user_id;
 			$row['name']     = $time->user->email;
 
-			fputcsv($f, $row, ',');
+		
+		fputcsv($out, array($row['time'] , $row['activity'] , $row['id'] ,$row['name']));
+
 		}
+		
+		fputcsv($out, array('this','is some', 'csv "stuff", you know.2'));
+		fclose($out);
+	
+	*/	
+	
+	/*
+
+		fputcsv($f, array('Minutes', 'Type', 'Id', 'Email'), ',');
+		
+	//	echo $times;
+			
+		foreach ($times as $time) {
+			$row = array();
+		
+			$row['time']     = $time->minutes;
+			$row['activity'] = $time->activity();
+			$row['id']       = $time->user_id;
+			$row['name']     = $time->user->email;
+ 			 
+		fputcsv($f, $row, ',');
+		
+		 echo $time->minutes; 
+		 echo $time->activity(); 
+		 echo $time->user_id;
+		 echo $time->user->email;
+	
+	
+//	echo "1 <br/>";
+
+		}
+*/
+			 
+		
+
 	}
 
 	public function range()
