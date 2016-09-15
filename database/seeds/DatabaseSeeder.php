331c8a6e35aab9cbbaf3667d902f82f2e29e2f1c
Ken Lett <kennric@osuosl.org>
2016-09-15 09:56:13 -0700

    take users seed out of repo

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -22,7 +22,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(LanguagesTableSeeder::class);
         $this->call(SchoolsTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
         $this->call(PagesTableSeeder::class);
         $this->call(PagesModulesTableSeeder::class);
         $this->call(ChallengesTableSeeder::class);
