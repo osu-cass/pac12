3a8641ff38810cedf136ba422fd6c79fd954509f
Aaron Cohen <cohenaa@osuosl.org>
2016-10-18 17:09:31 -0700

    Remove things on front page with hardcoded dates

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -15,97 +15,57 @@
 {{ HTML::script('assets/jqplot/plugins/jqplot.pointLabels.min.js') }}
 <script>
     $(document).ready(function() {
-            $.jqplot.config.enablePlugins = true;
-            var options = {
-                // Only animate if we're not using excanvas (not in IE 7 or IE 8)..
-                animate: !$.jqplot.use_excanvas,
-                seriesDefaults:{
-                    renderer:$.jqplot.BarRenderer,
-                    pointLabels: { show: true }
-                },
-                axesDefaults: {
-                    tickOptions: {
-                        showGridline: false
-                    }
-                },
-                axes: {
-                    xaxis: {
-                        renderer: $.jqplot.CategoryAxisRenderer,
-                        ticks: []
-                    },
-                    yaxis: {
-                        showTicks: false,
-                        rendererOptions: {
-                            drawBaseline: false
-                        }
-                    }
-                },
-                highlighter: {
-                    show: false
-                },
-                grid: {
-                    background: 'rgba(0, 0, 0, 0)',
-                    borderWidth: 0
+        $.jqplot.config.enablePlugins = true;
+        var options = {
+            // Only animate if we're not using excanvas (not in IE 7 or IE 8)..
+            animate: !$.jqplot.use_excanvas,
+            seriesDefaults:{
+                renderer:$.jqplot.BarRenderer,
+                pointLabels: { show: true }
+            },
+            axesDefaults: {
+                tickOptions: {
+                    showGridline: false
                 }
-            };
+            },
+            axes: {
+                xaxis: {
+                    renderer: $.jqplot.CategoryAxisRenderer,
+                    ticks: []
+                },
+                yaxis: {
+                    showTicks: false,
+                    rendererOptions: {
+                        drawBaseline: false
+                    }
+                }
+            },
+            highlighter: {
+                show: false
+            },
+            grid: {
+                background: 'rgba(0, 0, 0, 0)',
+                borderWidth: 0
+            }
+        };
 
-            var plot0Options = options;
-            plot0Options.axes.xaxis.ticks = ['','','','','','','','','','','',''];
-            var plot0Data = [
-                {{ $totals[0]->minutes }},
+        var plot0Options = options;
+        plot0Options.axes.xaxis.ticks = ['','','','','','','','','','','',''];
+        var plot0Data = [
+        {{ $totals[0]->minutes }},
         {{ $totals[1]->minutes }},
-    {{ $totals[2]->minutes }},
-    {{ $totals[3]->minutes }},
-    {{ $totals[4]->minutes }},
-    {{ $totals[5]->minutes }},
-    {{ $totals[6]->minutes }},
-    {{ $totals[7]->minutes }},
-    {{ $totals[8]->minutes }},
-    {{ $totals[9]->minutes }},
-    {{ $totals[10]->minutes }},
-    {{ $totals[11]->minutes }}
-    ];
-    var plot0 = $.jqplot('chart', [plot0Data], options);
-
-    @if(Carbon::now()->toDateString() == '2014-02-27' || Input::get('graphs'))
-    var plot1Options = options;
-    plot1Options.axes.xaxis.ticks = ['',''];
-    var plot1Data = [
-        {{ $totals[1]->minutes }},
-    {{ $totals[0]->minutes }}
-    ];
-    var plot1 = $.jqplot('chart1', [plot1Data], plot1Options);
-
-    var plot2Data = [
+        {{ $totals[2]->minutes }},
         {{ $totals[3]->minutes }},
-    {{ $totals[4]->minutes }}
-    ];
-    var plot2 = $.jqplot('chart2', [plot2Data], plot1Options);
-
-    var plot3Data = [
-        {{ $totals[8]->minutes }},
-    {{ $totals[9]->minutes }}
-    ];
-    var plot3 = $.jqplot('chart3', [plot3Data], plot1Options);
-
-    var plot4Data = [
-        {{ $totals[6]->minutes }},
-    {{ $totals[7]->minutes }}
-    ];
-    var plot4 = $.jqplot('chart4', [plot4Data], plot1Options);
-
-    var plot5Data = [
+        {{ $totals[4]->minutes }},
         {{ $totals[5]->minutes }},
-    {{ $totals[2]->minutes }}
-    ];
-    var plot5 = $.jqplot('chart5', [plot5Data], plot1Options);
-
-    var plot6Data = [
-        {{ $totals[11]->minutes }},
-    {{ $totals[10]->minutes }}
-    ];
-    var plot6 = $.jqplot('chart6', [plot6Data], plot1Options);
-    @endif
+        {{ $totals[6]->minutes }},
+        {{ $totals[7]->minutes }},
+        {{ $totals[8]->minutes }},
+        {{ $totals[9]->minutes }},
+        {{ $totals[10]->minutes }},
+        {{ $totals[11]->minutes }}
+        ];
+        var plot0 = $.jqplot('chart', [plot0Data], options);
     });
 </script>
 @stop
@@ -115,11 +75,7 @@
 @stop
 
 @section('content')
-<div class="section chart<?php
-if (time() < strtotime('2014-02-24') && !Input::get('graphs')) {
-    echo ' cripple';
-}
-?>">
+<div class="section chart">
     <h1 class="text-center">STUDENTS REGISTERED</h1>
     <div id="chart">
     </div>
@@ -165,72 +121,7 @@ if (time() < strtotime('2014-02-24') && !Input::get('graphs')) {
         </div>
     </div>
 @endif
-@if(Carbon::now()->toDateString() == '2014-02-27' || Input::get('graphs'))
-<div class="section chart2">
-    <div class="row">
-        <div class="col-xs-2">
-            <div class="pad">
-                <div id="chart1" class="duelChart">
-                </div>
-                <div class="text-center">
-                    <div class="miniIcon icon2"></div>
-                    <div class="miniIcon icon1"></div>
-                </div>
-            </div>
-        </div>
-        <div class="col-xs-2">
-            <div class="pad">
-                <div id="chart2" class="duelChart">
-                </div>
-                <div class="text-center">
-                    <div class="miniIcon icon4"></div>
-                    <div class="miniIcon icon5"></div>
-                </div>
-            </div>
-        </div>
-        <div class="col-xs-2">
-            <div class="pad">
-                <div id="chart3" class="duelChart">
-                </div>
-                <div class="text-center">
-                    <div class="miniIcon icon9"></div>
-                    <div class="miniIcon icon10"></div>
-                </div>
-            </div>
-        </div>
-        <div class="col-xs-2">
-            <div class="pad">
-                <div id="chart4" class="duelChart">
-                </div>
-                <div class="text-center">
-                    <div class="miniIcon icon7"></div>
-                    <div class="miniIcon icon8"></div>
-                </div>
-            </div>
-        </div>
-        <div class="col-xs-2">
-            <div class="pad">
-                <div id="chart5" class="duelChart">
-                </div>
-                <div class="text-center">
-                    <div class="miniIcon icon6"></div>
-                    <div class="miniIcon icon3"></div>
-                </div>
-            </div>
-        </div>
-        <div class="col-xs-2">
-            <div class="pad">
-                <div id="chart6" class="duelChart">
-                </div>
-                <div class="text-center">
-                    <div class="miniIcon icon12"></div>
-                    <div class="miniIcon icon11"></div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endif
+
 <div class="section s3">
     <img src="{{ asset('assets/images/section-3.png') }}" alt="" class="section-divider">
     {!! $page->get_module_by_number(1) !!}
@@ -238,18 +129,6 @@ if (time() < strtotime('2014-02-24') && !Input::get('graphs')) {
         @foreach($schools as $school)
             <a class="icon icon{{ $school->id }}" href="{{ $school->url }}" target="_blank"></a>
         @endforeach
-        {{--<a class="icon icon1" href="http://rec.arizona.edu/" target="_blank"></a>--}}
-        {{--<a class="icon icon2" href="https://fitness.asu.edu/" target="_blank"></a>--}}
-        {{--<a class="icon icon3" href="http://recsports.berkeley.edu/about/events/fitwellweek/" target="_blank"></a>--}}
-        {{--<a class="icon icon4" href="http://uoregon.edu/sports" target="_blank"></a>--}}
-        {{--<a class="icon icon5" href="http://calendar.oregonstate.edu/" target="_blank"></a>--}}
-        {{--<a class="icon icon6" href="http://www.stanford.edu/dept/pe/cgi-bin/cardinalrec/pac-12-fitness-challenge/" target="_blank"></a>--}}
-        {{--<a class="icon icon7" href="http://www.recreation.ucla.edu/pac12challenge" target="_blank"></a>--}}
-        {{--<a class="icon icon8" href="http://sait.usc.edu/Recsports/calendars/sports/pac-12-fitness-challenge-week-2014" target="_blank"></a>--}}
-        {{--<a class="icon icon9" href="http://www.washington.edu/ima" target="_blank"></a>--}}
-        {{--<a class="icon icon10" href="http://www.urec.wsu.edu/events/pac-12-challenge/" target="_blank"></a>--}}
-        {{--<a class="icon icon11" href="http://www.colorado.edu/recreation" target="_blank"></a>--}}
-        {{--<a class="icon icon12" href="http://campusrec.utah.edu/" target="_blank"></a>--}}
     </div>
     <p>For PAC-12 Challenge Events On Your Campus.</p>
 </div>
